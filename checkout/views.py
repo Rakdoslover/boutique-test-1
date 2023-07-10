8815cb3ef7d513819452b848ecbb6d9deab4b59b
Rakdoslover <>
2023-07-10 16:02:56 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,7 +13,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NSM1mC2OyRDzEXjxrj5pAvjTznV3QJ1pBHIReRwYTaW9mEICCcckGeim4mMNZRtB3FW7dxLxlyFFlg42GLbQLkB00yT3WD41u',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
