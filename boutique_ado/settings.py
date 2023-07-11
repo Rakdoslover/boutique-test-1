f2d191f6ba21a51936f275e4d9dfb928be6ac0ab
Rakdoslover <>
2023-07-11 09:06:17 +0000

    added bsic checkout functionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+if os.path.exists("env.py"):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e)jwmv^r!61^tbs(-df&5oo-_0y=(5j$jgt6%-z6d4hg0jl!35'
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- checkout/views.py
+++ checkout/views.py
@@ -28,6 +28,8 @@ def checkout(request):
 
     order_form = OrderForm()
 
+    print(intent)
+
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
             Did you forget to set it in your environment?')
