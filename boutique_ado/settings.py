0fd4cc6c64bf0ad40042a62ae5517b4f80ef158c
Rakdoslover <>
2023-07-12 14:58:16 +0000

    completed confirmation emails

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-rakdoslover-boutique-tes-qguqpdvgqg.us2.codeanyapp.com', ]
+ALLOWED_HOSTS = [
+
+    '8000-rakdoslover-boutique-tes-qguqpdvgqg.us2.codeanyapp.com', ]
 
 
 # Application definition
