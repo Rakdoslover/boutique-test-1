87883da23d11749fc5ea0c231c57f3de0ff53c1a
Rakdoslover <>
2023-07-12 11:03:46 +0000

    completed webhook handlers

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
@@ -11,6 +11,7 @@ from bag.contexts import bag_contents
 import stripe
 import json
 
+
 @require_POST
 def cache_checkout_data(request):
     try:
@@ -20,13 +21,12 @@ def cache_checkout_data(request):
             'bag': json.dumps(request.session.get('bag', {})),
             'save_info': request.POST.get('save_info'),
             'username': request.user,
-            
         })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, 'Sorry, you payment cannot be \
-            processed right now. Please try again later.' )
-        return HttpResponse(context=e, status=400)       
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
